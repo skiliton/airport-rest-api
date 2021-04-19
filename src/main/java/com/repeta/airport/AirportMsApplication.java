ad872800f07db3878d638185fcf38792d7619021
Ivan Repeta <ivan.repeta.work@gmail.com>
2021-04-19 17:03:54 +0300

    Initial commit

---
+++ src/main/java/com/repeta/airport/AirportMsApplication.java
@@ -0,0 +1,13 @@
+package com.repeta.airport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportMsApplication.class, args);
+    }
+
+}
